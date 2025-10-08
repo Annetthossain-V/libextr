bb6dfe3617267166a73eaa3680aaf6b67a422b30
Annett Hossain <annetthossain@outlook.com>
2025-10-08 21:42:02 +0600

    bug fix. split_cxx did not check for null array

--- src/string/split.cxx
+++ src/string/split.cxx
@@ -1,12 +1,18 @@
 #include "extr_string.h"
+#include <stdexcept>
 #include <vector>
 #include <string>
+#include "../core/config.h"
 
 std::vector<std::string> extr::split_tokens_cxx(std::string str, std::string delims) {
   std::vector<std::string> tokens;
 
   size_t count = 0;
   char** c_tokens = split_tokens(str.c_str(), delims.c_str(), &count);
+  if (c_tokens == NULL) {
+    if (ENABLE_EXCEPTION) throw std::runtime_error("unable to split tokens");
+    return tokens;
+  }
 
   tokens.reserve(count);
   for (size_t i = 0; i < count; i++) {
